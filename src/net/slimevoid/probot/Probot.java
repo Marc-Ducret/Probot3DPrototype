f7a8b016e8e6a1f2d753629527a46419f208add1
VengeurK <vengeurk@gmail.com>
2017-03-03 19:19:51 +0100

    Refactoring

--- src/net/slimevoid/probot/Probot.java
+++ src/net/slimevoid/probot/Probot.java
@@ -4,7 +4,7 @@ import net.slimevoid.gl.GLInterface;
 
 public class Probot {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		GLInterface.start(720, 512, "Probot!", false);
 	}
 }
